57dbc3705d426429eccc78357460590f6c68ddf1
arun3688 <rain100falls@gmail.com>
2018-05-03 15:26:33 +0200

    create tempdir for each omcsession

--- OMJulia.jl
+++ OMJulia.jl
@@ -45,6 +45,8 @@ type OMCSession
    simulationFlag
    simulateOptions
    overridevariables
+   tempdir
+   currentdir
    resultfile
    filepath
    modelname
@@ -55,14 +57,16 @@ type OMCSession
    socket
    function OMCSession()
       this = new()
-	  this.overridevariables=Dict()
+      this.overridevariables=Dict()
       this.quantitieslist=Any[]
       this.parameterlist=Dict()
-	  this.simulateOptions=Dict()
-	  this.filepath=""
-	  this.modelname=""
-	  this.resultfile=""
-	  this.simulationFlag=""
+      this.simulateOptions=Dict()
+      this.currentdir=pwd()
+      this.filepath=""
+      this.modelname=""
+      this.resultfile=""
+      this.simulationFlag=""
+      this.tempdir=""
       args2="--interactive=zmq"
       args3="+z=julia."
       args4=randstring(10)
@@ -97,28 +101,30 @@ type OMCSession
       end
 
       this.ModelicaSystem = function (filename, modelname)
-	     this.filepath=filename
-		 this.modelname=modelname
-		 filepath=replace(abspath(filename),"\\","/")
-		 #println(filepath)
-		 if(isfile(filepath))		    
-			 loadmsg=this.sendExpression("loadFile(\""*filepath*"\")")
-			 if(!parse(loadmsg))
-				return this.sendExpression("getErrorString()")
-			 end
+         this.filepath=filename
+         this.modelname=modelname
+         filepath=replace(abspath(filename),"\\","/")
+         #println(filepath)
+         if(isfile(filepath))
+            loadmsg=this.sendExpression("loadFile(\""*filepath*"\")")
+            if(!parse(loadmsg))
+               return this.sendExpression("getErrorString()")
+            end
          else
-             return println(filename, "! NotFound")
-         end 
-		 
-		 buildmodelexpr=join(["buildModel(",modelname,")"])
-		 buildModelmsg=this.sendExpression(buildmodelexpr)
-		 parsebuilexp=parse(buildModelmsg)
-		 if(!isempty(parsebuilexp.args[2]))
-		     this.xmlfile=replace(joinpath(pwd(),parsebuilexp.args[2]),"\\","/")
-	         xmlparse(this)
-		 else
-		     return this.sendExpression("getErrorString()")
-		 end 
+            return println(filename, "! NotFound")
+         end
+         this.tempdir=replace(mktempdir(pwd()),"\\","/")
+         this.sendExpression("cd(\""*this.tempdir*"\")")
+         buildmodelexpr=join(["buildModel(",modelname,")"])
+         buildModelmsg=this.sendExpression(buildmodelexpr)
+         parsebuilexp=parse(buildModelmsg)
+
+         if(!isempty(parsebuilexp.args[2]))
+            this.xmlfile=replace(joinpath(this.tempdir,parsebuilexp.args[2]),"\\","/")
+            xmlparse(this)
+         else
+            return this.sendExpression("getErrorString()")
+         end
          # ZMQ.send(this.socket,buildmodelexpr)
          # message=ZMQ.recv(this.socket)
          # this.xmlfile=joinpath(pwd(),join([modelname,"_init.xml"]))
@@ -131,14 +137,14 @@ type OMCSession
          elseif(isa(name,String))
             return [x for x in this.quantitieslist if x["name"] == name]
          elseif (isa(name,Array))
-		    return [x for y in name for x in this.quantitieslist if x["name"]==y]
+            return [x for y in name for x in this.quantitieslist if x["name"]==y]
             # qlist=Any[]
             # for n in name
-               # for i in this.quantitieslist
-                  # if(i["name"]==n)
-                     # push!(qlist,i)
-                  # end
-               # end
+            # for i in this.quantitieslist
+            # if(i["name"]==n)
+            # push!(qlist,i)
+            # end
+            # end
             # end
             # return qlist
          end
@@ -147,129 +153,133 @@ type OMCSession
       this.getParameters = function (name=nothing)
          if(name==nothing)
             return this.parameterlist
-		 elseif(isa(name,String))
-		    return get(this.parameterlist,name,0)
+         elseif(isa(name,String))
+            return get(this.parameterlist,name,0)
          elseif (isa(name,Array))
             return [get(this.parameterlist,x,0) for x in name]
          end
       end
-      
-	  this.getSimulationOptions = function (name=nothing)
-		 if (name==nothing)
-		    return this.simulateOptions
-		 elseif(isa(name,String))
-		    return get(this.simulateOptions,name,0)
-		 elseif(isa(name,Array))
-		    return [get(this.simulateOptions,x,0) for x in name]
-         end 
-	  end 
-	  
-	  this.simulate = function()
-	      println(this.xmlfile)
-		  if(isfile(this.xmlfile))
-			  if (Compat.Sys.iswindows())
-			     getexefile=replace(joinpath(pwd(),join([this.modelname,".exe"])),"\\","/")
-			  else
-			     getexefile=replace(joinpath(pwd(),this.modelname),"\\","/")
-			  end
-			  if(isfile(getexefile))
-			    if(!isempty(this.overridevariables))
-					overridelist=Any[]
-					for k in keys(this.overridevariables)
-						val=join([k,"=",this.overridevariables[k]])
-						push!(overridelist,val)
-					end
-					#println(overridelist)
-				    overridevar=join(["-override=",join(overridelist,",")])
-					#println(overridevar)	
-                    run(`$getexefile $overridevar`)					
-				else				
-				   run(`$getexefile`)
-				end 
-				this.resultfile=replace(joinpath(pwd(),join([this.modelname,"_res.mat"])),"\\","/")
-				this.simulationFlag="True"
-              else
-                return println("! Simulation Failed")
-              end				
-		  end 
-	  end 
-	  
-	  this.getSolutions = function(name=nothing)
-		   if(!isempty(this.resultfile))
-		      if(name==nothing)
-			     simresultvars=this.sendExpression("readSimulationResultVars(\"" * this.resultfile * "\")")
-				 parsesimresultvars=parse(simresultvars)
-				 return parsesimresultvars.args
-			  elseif(isa(name,String))
-			     resultvar=join(["{",name,"}"])
-                 simres=this.sendExpression("readSimulationResult(\""* this.resultfile * "\","* resultvar *")")
-				 data=parse(simres)
-				 this.sendExpression("closeSimulationResultFile()")
-                 return [plotdata.args for plotdata in data.args]				 
-			  elseif(isa(name,Array))
-			     resultvar=join(["{",join(name,","),"}"])
-				 #println(resultvar)
-			     simres=this.sendExpression("readSimulationResult(\""* this.resultfile * "\","* resultvar *")")
-				 data=parse(simres)
-				 plotdata=Any[]
-				 for item in data.args
-					push!(plotdata,item.args)
-				 end 
-				 this.sendExpression("closeSimulationResultFile()")
-				 return plotdata
-			  end 
-		   else
-		      return println("Model not Simulated, Simulate the model to get the results")
-		   end
-	  end 
-	  
-	  this.setParameters = function (name)
-	     if(isa(name,String))
-			 value=split(name,"=")
-		     #setxmlfileexpr="setInitXmlStartValue(\""* this.xmlfile * "\",\""* value[1]* "\",\""*value[2]*"\",\""*this.xmlfile*"\")"
-			 #println(haskey(this.parameterlist, value[1]))
-			 if(haskey(this.parameterlist,value[1]))
-			      this.parameterlist[value[1]]=value[2]
-				  this.overridevariables[value[1]]=value[2]
-			 else
-			      return println(value[1], "is not a parameter")
-		     end 
-			 #this.sendExpression(setxmlfileexpr)
-	     elseif(isa(name,Array))
-			for var in name
-			     value=split(var,"=")
-		         if(haskey(this.parameterlist,value[1]))
-					this.parameterlist[value[1]]=value[2]
-					this.overridevariables[value[1]]=value[2]
-				else
-					return println(value[1], "is not a parameter")
-		        end 
-			end 
-		 end 		    
-	  end
-	  
+
+      this.getSimulationOptions = function (name=nothing)
+         if (name==nothing)
+            return this.simulateOptions
+         elseif(isa(name,String))
+            return get(this.simulateOptions,name,0)
+         elseif(isa(name,Array))
+            return [get(this.simulateOptions,x,0) for x in name]
+         end
+      end
+
+      this.simulate = function()
+         println(this.xmlfile)
+         if(isfile(this.xmlfile))
+            if (Compat.Sys.iswindows())
+               getexefile=replace(joinpath(this.tempdir,join([this.modelname,".exe"])),"\\","/")
+            else
+               getexefile=replace(joinpath(this.tempdir,this.modelname),"\\","/")
+            end
+            if(isfile(getexefile))
+               ## change to tempdir
+               cd(this.tempdir)
+               if(!isempty(this.overridevariables))
+                  overridelist=Any[]
+                  for k in keys(this.overridevariables)
+                     val=join([k,"=",this.overridevariables[k]])
+                     push!(overridelist,val)
+                  end
+                  #println(overridelist)
+                  overridevar=join(["-override=",join(overridelist,",")])
+                  #println(overridevar)
+                  run(`$getexefile $overridevar`)
+               else
+                  run(`$getexefile`)
+               end
+               this.resultfile=replace(joinpath(this.tempdir,join([this.modelname,"_res.mat"])),"\\","/")
+               this.simulationFlag="True"
+            else
+               return println("! Simulation Failed")
+            end
+            ## change to currentworkingdirectory
+            cd(this.currentdir)
+         end
+      end
+
+      this.getSolutions = function(name=nothing)
+         if(!isempty(this.resultfile))
+            if(name==nothing)
+               simresultvars=this.sendExpression("readSimulationResultVars(\"" * this.resultfile * "\")")
+               parsesimresultvars=parse(simresultvars)
+               return parsesimresultvars.args
+            elseif(isa(name,String))
+               resultvar=join(["{",name,"}"])
+               simres=this.sendExpression("readSimulationResult(\""* this.resultfile * "\","* resultvar *")")
+               data=parse(simres)
+               this.sendExpression("closeSimulationResultFile()")
+               return [plotdata.args for plotdata in data.args]
+            elseif(isa(name,Array))
+               resultvar=join(["{",join(name,","),"}"])
+               #println(resultvar)
+               simres=this.sendExpression("readSimulationResult(\""* this.resultfile * "\","* resultvar *")")
+               data=parse(simres)
+               plotdata=Any[]
+               for item in data.args
+                  push!(plotdata,item.args)
+               end
+               this.sendExpression("closeSimulationResultFile()")
+               return plotdata
+            end
+         else
+            return println("Model not Simulated, Simulate the model to get the results")
+         end
+      end
+
+      this.setParameters = function (name)
+         if(isa(name,String))
+            value=split(name,"=")
+            #setxmlfileexpr="setInitXmlStartValue(\""* this.xmlfile * "\",\""* value[1]* "\",\""*value[2]*"\",\""*this.xmlfile*"\")"
+            #println(haskey(this.parameterlist, value[1]))
+            if(haskey(this.parameterlist,value[1]))
+               this.parameterlist[value[1]]=value[2]
+               this.overridevariables[value[1]]=value[2]
+            else
+               return println(value[1], "is not a parameter")
+            end
+            #this.sendExpression(setxmlfileexpr)
+         elseif(isa(name,Array))
+            for var in name
+               value=split(var,"=")
+               if(haskey(this.parameterlist,value[1]))
+                  this.parameterlist[value[1]]=value[2]
+                  this.overridevariables[value[1]]=value[2]
+               else
+                  return println(value[1], "is not a parameter")
+               end
+            end
+         end
+      end
+
       this.setSimulationOptions = function (name)
-		 if(isa(name,String))
-			 value=split(name,"=")
-			 if(haskey(this.simulateOptions,value[1]))
-			      this.simulateOptions[value[1]]=value[2]
-				  this.overridevariables[value[1]]=value[2]
-			 else
-			      return println(value[1], "is not a SimulationOption")
-		     end 
-	     elseif(isa(name,Array))
-			for var in name
-			     value=split(var,"=")
-		         if(haskey(this.simulateOptions,value[1]))
-					this.simulateOptions[value[1]]=value[2]
-					this.overridevariables[value[1]]=value[2]
-				else
-					return println(value[1], "is not a SimulationOption")
-		        end 
-			end 
-		 end
-      end	
-	  
+         if(isa(name,String))
+            value=split(name,"=")
+            if(haskey(this.simulateOptions,value[1]))
+               this.simulateOptions[value[1]]=value[2]
+               this.overridevariables[value[1]]=value[2]
+            else
+               return println(value[1], "is not a SimulationOption")
+            end
+         elseif(isa(name,Array))
+            for var in name
+               value=split(var,"=")
+               if(haskey(this.simulateOptions,value[1]))
+                  this.simulateOptions[value[1]]=value[2]
+                  this.overridevariables[value[1]]=value[2]
+               else
+                  return println(value[1], "is not a SimulationOption")
+               end
+            end
+         end
+      end
+
       function xmlparse(this)
          if(isfile(this.xmlfile))
             xdoc = parse_file(this.xmlfile)
@@ -278,13 +288,13 @@ type OMCSession
             for c in child_nodes(xroot)  # c is an instance of XMLNode
                if is_elementnode(c)
                   e = XMLElement(c)  # this makes an XMLElement instance
-				  if(name(e)=="DefaultExperiment")		
-				    this.simulateOptions["startTime"]=attribute(e, "startTime")
-					this.simulateOptions["stopTime"]=attribute(e, "stopTime")
-					this.simulateOptions["stepSize"]=attribute(e, "stepSize")
-					this.simulateOptions["tolerance"]=attribute(e, "tolerance")
-					this.simulateOptions["solver"]=attribute(e, "solver")				
-				  end 
+                  if(name(e)=="DefaultExperiment")
+                     this.simulateOptions["startTime"]=attribute(e, "startTime")
+                     this.simulateOptions["stopTime"]=attribute(e, "stopTime")
+                     this.simulateOptions["stepSize"]=attribute(e, "stepSize")
+                     this.simulateOptions["tolerance"]=attribute(e, "tolerance")
+                     this.simulateOptions["solver"]=attribute(e, "solver")
+                  end
                   if(name(e)=="ModelVariables")
                      for r in child_elements(e)
                         scalar = Dict()
