b4bdf0af08f0cda7c749efbc1e9180f73cef9676
Martin Sjölund <martin.sjolund@liu.se>
2019-02-19 14:36:51 +0100

    Add Parser module
    Also added a Project.toml and unit testing.

---
+++ test/runtests.jl
@@ -0,0 +1,45 @@
+module TestOMJulia
+
+using OMJulia
+using Test
+
+@testset "OMJulia" begin
+
+@testset "Parser" begin
+
+function check(string, expected_value, expected_type)
+  value = OMJulia.Parser.parse_whole(OMJulia.Parser.exp, string)
+  expected_value == value && expected_type == typeof(value)
+end
+
+@test check("123.0", 123.0, Float64)
+@test check("123", 123, Int)
+@test check("1.", 1.0, Float64)
+@test check(".2", 0.2, Float64)
+@test check("1e3", 1e3, Float64)
+@test check("1e+2", 1e+2, Float64)
+@test check("tRuE", true, Bool)
+@test check("false", false, Bool)
+@test check("\"ab\\nc\"", "ab\nc", String)
+@test check("{1,2,3}", [1,2,3], Array{Int,1})
+@test check("(1,2,3)", (1,2,3), Tuple{Int,Int,Int})
+@test check("NONE()", nothing, Nothing)
+@test check("SOME(1)", 1, Int)
+@test check("abc_2", "abc_2", String)
+@test check("record ABC end ABC;", Dict(), Dict{String,Any})
+@test check("record ABC a = 1, 'b' = 2,\n  c = 3\nend ABC;", Dict("a" => 1, "'b'" => 2, "c" => 3), Dict{String,Int})
+
+end
+
+end
+
+@testset "OpenModelica" begin
+
+omc = OMJulia.OMCSession()
+
+@test "3\n"==omc.sendExpression("1+2")
+@test 3==OMJulia.sendExpression(omc, "1+2")
+
+end
+
+end
