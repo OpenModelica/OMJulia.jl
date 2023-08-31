662c560ffe0ad0edad903ce1fa055f0b04486617
AnHeuermann <38031952+AnHeuermann@users.noreply.github.com>
2023-08-31 11:02:35 +0200

    Adding simulation tests

---
+++ test/omcTest.jl
@@ -0,0 +1,93 @@
+#=
+This file is part of OpenModelica.
+Copyright (c) 1998-2023, Open Source Modelica Consortium (OSMC),
+c/o Linköpings universitet, Department of Computer and Information Science,
+SE-58183 Linköping, Sweden.
+
+All rights reserved.
+
+THIS PROGRAM IS PROVIDED UNDER THE TERMS OF THE BSD NEW LICENSE OR THE
+GPL VERSION 3 LICENSE OR THE OSMC PUBLIC LICENSE (OSMC-PL) VERSION 1.2.
+ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS PROGRAM CONSTITUTES
+RECIPIENT'S ACCEPTANCE OF THE OSMC PUBLIC LICENSE OR THE GPL VERSION 3,
+ACCORDING TO RECIPIENTS CHOICE.
+
+The OpenModelica software and the OSMC (Open Source Modelica Consortium)
+Public License (OSMC-PL) are obtained from OSMC, either from the above
+address, from the URLs: http://www.openmodelica.org or
+http://www.ida.liu.se/projects/OpenModelica, and in the OpenModelica
+distribution. GNU version 3 is obtained from:
+http://www.gnu.org/copyleft/gpl.html. The New BSD License is obtained from:
+http://www.opensource.org/licenses/BSD-3-Clause.
+
+This program is distributed WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, EXCEPT AS
+EXPRESSLY SET FORTH IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE
+CONDITIONS OF OSMC-PL.
+=#
+
+using Test
+import OMJulia
+
+@testset "OpenModelica" begin
+    @testset "OMCSession" begin
+        workdir = abspath(joinpath(@__DIR__, "test-session"))
+        rm(workdir, recursive=true, force=true)
+        mkpath(workdir)
+
+        oldwd = pwd()
+        try
+            cd(workdir)
+
+            omc = OMJulia.OMCSession()
+            version = OMJulia.sendExpression(omc, "getVersion()")
+            @test startswith(version, "v1.")
+            a = OMJulia.sendExpression(omc, "model a end a;")
+            @test a == [:a]
+
+            classNames = OMJulia.sendExpression(omc, "getClassNames()")
+            @test classNames == [:a]
+            @test true == OMJulia.sendExpression(omc, "loadModel(Modelica)")
+            res = OMJulia.sendExpression(omc, "simulate(Modelica.Electrical.Analog.Examples.CauerLowPassAnalog)")
+            @test isfile(res["resultFile"])
+            @test occursin("The simulation finished successfully.", res["messages"])
+
+            @test 3 == OMJulia.sendExpression(omc, "1+2")
+
+            ret = OMJulia.sendExpression(omc, "quit()", parsed=false)
+            @test ret == "quit requested, shutting server down\n"
+        finally
+            cd(oldwd)
+        end
+    end
+
+    @testset "Multiple sessions" begin
+        omc1 = OMJulia.OMCSession()
+        omc2 = OMJulia.OMCSession()
+
+        workdir1 = abspath(joinpath(@__DIR__, "test-omc1"))
+        workdir2 = abspath(joinpath(@__DIR__, "test-omc2"))
+        rm(workdir1, recursive=true, force=true)
+        rm(workdir2, recursive=true, force=true)
+        mkpath(workdir1)
+        mkpath(workdir2)
+
+        if Sys.iswindows()
+            workdir1 = replace(workdir1, "\\" => "\\\\")
+            workdir2 = replace(workdir2, "\\" => "\\\\")
+        end
+
+        OMJulia.sendExpression(omc1, "cd(\"$workdir1\")")
+        @test true == OMJulia.sendExpression(omc1, "loadModel(Modelica)")
+        res = OMJulia.sendExpression(omc1, "simulate(Modelica.Blocks.Examples.PID_Controller)")
+        @test isfile(joinpath(@__DIR__, "test-omc1", "Modelica.Blocks.Examples.PID_Controller_res.mat"))
+
+        OMJulia.sendExpression(omc2, "cd(\"$workdir2\")")
+        @test true == OMJulia.sendExpression(omc2, "loadModel(Modelica)")
+        res = OMJulia.sendExpression(omc2, "simulate(Modelica.Blocks.Examples.PID_Controller)")
+        @test isfile(joinpath(@__DIR__, "test-omc2", "Modelica.Blocks.Examples.PID_Controller_res.mat"))
+
+        OMJulia.sendExpression(omc1, "quit()", parsed=false)
+        OMJulia.sendExpression(omc2, "quit()", parsed=false)
+    end
+end
--- test/parserTest.jl
+++ test/parserTest.jl
@@ -26,10 +26,31 @@ EXPRESSLY SET FORTH IN THE BY RECIPIENT SELECTED SUBSIDIARY LICENSE
 CONDITIONS OF OSMC-PL.
 =#
 
-using SafeTestsets
-using Test
+using OMJulia
 
-@testset "OMJulia" begin
-    @safetestset "Parsing" begin include("parserTest.jl") end
-    @safetestset "OMCSession" begin include("omcTest.jl") end
+function check(string, expected_value, expected_type)
+    value = OMJulia.Parser.parseOM(string)
+    return expected_value == value && expected_type == typeof(value)
+end
+
+@testset "Parser" begin
+    @test check("123.0", 123.0, Float64)
+    @test check("123", 123, Int)
+    @test check("1.", 1.0, Float64)
+    @test check(".2", 0.2, Float64)
+    @test check("1e3", 1e3, Float64)
+    @test check("1e+2", 1e+2, Float64)
+    @test check("tRuE", true, Bool)
+    @test check("false", false, Bool)
+    @test check("\"ab\\nc\"", "ab\nc", String)
+    @test check("{\"abc\"}", ["abc"], Array{String,1})
+    @test check("{1}", [1], Array{Int,1})
+    @test check("{1,2,3}", [1,2,3], Array{Int,1})
+    @test check("(1,2,3)", (1,2,3), Tuple{Int,Int,Int})
+    @test check("NONE()", nothing, Nothing)
+    @test check("SOME(1)", 1, Int)
+    @test check("abc_2", :abc_2, Symbol)
+    @test check("record ABC end ABC;", Dict(), Dict{String,Any})
+    @test check("record ABC a = 1, 'b' = 2,\n  c = 3\nend ABC;", Dict("a" => 1, "'b'" => 2, "c" => 3), Dict{String,Int})
+    @test check("", nothing, Nothing)
 end
